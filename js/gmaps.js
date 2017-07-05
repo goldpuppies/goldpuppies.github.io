eaaca645e2580423d104bcd9026f6309116375fc
goldpuppies <rdefo1@gmail.com>
2017-07-05 14:06:44 +0300

    add gmaps.js

--- js/gmaps.js
+++ js/gmaps.js
@@ -1248,7 +1248,7 @@ if(window.google && window.google.maps){
       var parameters = [];
       var data;
 
-      var static_root = 'http://maps.googleapis.com/maps/api/staticmap';
+      var static_root = 'https://maps.googleapis.com/maps/api/staticmap';
       if (options.url){
         static_root = options.url;
         delete options.url;
